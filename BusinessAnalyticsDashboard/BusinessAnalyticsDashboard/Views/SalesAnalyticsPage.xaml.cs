a567f3eefe08eddc7c7a0afd64fb612e3c46f239
sudarsanMS <sudarsan.m@syncfusion.com>
2024-11-05 20:22:22 +0530

    SaleAnalytics has been committed

--- BusinessAnalyticsDashboard/BusinessAnalyticsDashboard/Views/SalesAnalyticsPage.xaml.cs
+++ BusinessAnalyticsDashboard/BusinessAnalyticsDashboard/Views/SalesAnalyticsPage.xaml.cs
@@ -1,3 +1,5 @@
+using Syncfusion.Maui.Toolkit.Charts;
+
 namespace BusinessAnalyticsDashboard;
 
 public partial class SalesAnalyticsPage : ContentView
@@ -6,5 +8,13 @@ public partial class SalesAnalyticsPage : ContentView
 	{
 		InitializeComponent();
         BindingContext = new SalesAnalyticsViewModel();
+    }
+
+    private void SfChart_SizeChanged(object sender, EventArgs e)
+    {
+        if (sender is View chart)
+        {
+            chart.HeightRequest = Math.Max(chart.Width - 200, 200);
+        }
     }
 }
