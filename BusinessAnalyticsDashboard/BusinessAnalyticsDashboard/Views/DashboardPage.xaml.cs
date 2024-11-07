bc31f2c1cacb5cde64f198d0124061340c258e32
Paul Anderson <paulanderson@syncfusion.com>
2024-11-07 16:33:38 +0530

    Fixed the chart sizing issues

--- BusinessAnalyticsDashboard/BusinessAnalyticsDashboard/Views/DashboardPage.xaml.cs
+++ BusinessAnalyticsDashboard/BusinessAnalyticsDashboard/Views/DashboardPage.xaml.cs
@@ -7,12 +7,4 @@ public partial class DashboardPage : ContentView
 		InitializeComponent();
         BindingContext = new DashboardViewModel();
     }
-
-    private void SfChart_SizeChanged(object sender, EventArgs e)
-    {
-        if (sender is View chart)
-        {
-            chart.HeightRequest = Math.Max(chart.Width - 200, 200);
-        }
-    }
 }
